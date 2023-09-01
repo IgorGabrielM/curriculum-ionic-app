ff9778a06db8e5e78218c2acbba58a1b6bc98f28
igorGabrielM <igorgabprofissional@gmail.com>
2023-09-01 17:27:01 -0300

    fix: correct timer and center image of collaborator

--- src/app/menu/home/home.page.ts
+++ src/app/menu/home/home.page.ts
@@ -29,7 +29,7 @@ export class HomePage implements OnInit {
   ) { }
 
   ngOnInit() {
-    setTimeout(() => this.loadColaborator(), 500)
+    setTimeout(() => this.loadColaborator(), 5000)
   }
 
   loadColaborator() {
